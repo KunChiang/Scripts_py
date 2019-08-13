fad7ef322150dd0a8e524a5595419a0619293e8d
chiang <ichiangkun@gmail.com>
2019-08-13 11:02:40 +0800

    search times

--- unpack.py
+++ unpack.py
@@ -1,30 +1,32 @@
 # coding:utf-8
-import rarfile
 import argparse
+import multiprocessing as mp
 import os
 from itertools import permutations
+
+import rarfile
 from tqdm import tqdm
 
 FLAGS = None
-pwd_dict_file = "./pwd_dict.txt"
+pwd_dict_file = "./data/pwd_dict.txt"
 
 def unrar(filepath, pwd, savepath="./"):
     file = rarfile.RarFile(filepath)
     if savepath == 'rawpath':
         savepath = "/".join(savepath.split("/")[:-1])
-        print(savepath)
+        # print(savepath)
     elif savepath == 'thispath':
         savepath = "./"
     file.extractall(path=savepath, pwd=pwd)
     file.close()
 
-def search_pwd(file, targetpath, maxsearch=100000):
+def search_pwd(file, targetpath, maxsearch):
     count = 0
     zm = [chr(i) for i in range(33, 127)]
     lgths = range(4, 16)
-    for i in tqdm(lgths):
+    for i in tqdm(lgths, desc='pwd length'):
         pwds = [''.join(list(c)) for c in  permutations(zm, i)]
-        for pwd in tqdm(pwds):
+        for pwd in tqdm(pwds, desc='search space'):
             count += 1
             if count >= maxsearch:
                 return False, ''
@@ -37,6 +39,7 @@ def search_pwd(file, targetpath, maxsearch=100000):
 def main():
     file = FLAGS.filepath
     targetpath = FLAGS.targetpath
+    searchtimes = FLAGS.searchtimes
     file = ""
     pwd_dict = set()
     if os.path.exists(pwd_dict_file):
@@ -51,19 +54,21 @@ def main():
         except:
             pass
     if not unrar_flag:
-        unrar_flag, pwd = search_pwd(file, targetpath)
+        unrar_flag, pwd = search_pwd(file, targetpath, searchtimes)
 
     if unrar_flag:
         pwd_dic_f = open(pwd_dict_file, 'a')
         pwd_dic_f.write(pwd+'\n')
         pwd_dic_f.close()
-
-    print("Unrar success if: ", unrar_flag, 'pwd: ', pwd)
+        print("Unpack success! pwd: ", pwd, "savePath: ", targetpath)
+    else:
+        print("Unpack Failed!")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--filepath', type=str, default='', help='Help information')
     parser.add_argument('--targetpath', type=str, default='./', help='Help information')
+    parser.add_argument('--searchtimes', type=int, default=int(10e4), help='Help information')
     FLAGS, unparsed = parser.parse_known_args()
     main()
